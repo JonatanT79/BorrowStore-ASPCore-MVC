e2242fc6f1ec00d20545f260656f472a5cc8e67a
Jonatan Tran <jonathan.t79@hotmail.com>
2020-08-12 12:31:00 +0200

    Added Scaffolded Item

---
+++ BorrowStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BorrowStore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BorrowStore.Areas.Identity.IdentityHostingStartup))]
+namespace BorrowStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
