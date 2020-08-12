e2eaaef9c4b2d78b61cb03bf5233e39747397248
Jonatan Tran <jonathan.t79@hotmail.com>
2020-08-12 18:28:51 +0200

    Added MockData

---
+++ BorrowStore/Controllers/Products.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BorrowStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BorrowStore.Controllers
+{
+    public class Products : Controller
+    {
+        public IActionResult AllProducts()
+        {
+            var ProductList = TestData.ProductList();
+            return View(ProductList);
+        }
+    }
+
+    static class TestData
+    {
+        public static List<Product> ProductList()
+        {
+            List<Product> ProductList = new List<Product>()
+            {
+                new Product {ID = 1, Name = "Book", Description = "Cool Book"},
+                new Product {ID = 2, Name = "Spel", Description = "Action Game"}
+            };
+
+            return ProductList;
+        }
+    }
+}
---
+++ BorrowStore/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BorrowStore.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Exemplar
+    {
+        public string ExemplarID { get; set; } //PK
+        public int ProductID { get; set; } //FK
+        public bool Available { get; set; }
+    }
+}
+//Datum för inlämmning currunt date + 30 days
