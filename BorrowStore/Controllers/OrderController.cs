ea84ac4953fb476ddff9bd1159a31dc96ed22bae
Jonatan Tran <jonathan.t79@hotmail.com>
2020-08-14 01:42:03 +0200

    Implemented all big functions

---
+++ BorrowStore/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BorrowStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult ConfirmOrder(int ID)
+        {
+            var product = (from e in TestData.ProductList()
+                          where e.ID == ID
+                          select e).SingleOrDefault();
+
+            return View(product);
+        }
+        public IActionResult CompleteOrder()
+        {
+            return View();
+        }
+    }
+}
