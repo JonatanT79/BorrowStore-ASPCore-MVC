764bf1f97e78905d8ac2451b4d9d90d169194462
Jonatan Tran <jonathan.t79@hotmail.com>
2020-08-27 20:50:49 +0200

    Added Product API

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetProducts()
+        {
+            string[] arr = new string[] { "1", "2", "3" };
+            return Ok(arr);
+        }
+    }
+}
